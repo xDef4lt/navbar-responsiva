9dfd6e8c98bca24481f77d17a119c4379203ceb5
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-02-22 15:39:05 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const menuIcon = document.querySelectorAll('[data-animar]');
+const hamburguer = document.querySelector('div#btn');
+const animation = 'animar';
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+
+function MenuMobile () {
+    menuIcon.forEach (function (element) {
+        if (!element.classList.contains(animation)) {
+            element.classList.add(animation);
+            header.classList.add(animation);
+            nav.style.display = 'flex';
+            document.querySelector('.logo').style.display = 'none';
+        } else {
+            element.classList.remove(animation);
+            header.classList.remove(animation);
+            nav.style.display = 'none';
+            document.querySelector('.logo').style.display = 'flex';
+        }
+    });
+}
+
+document.querySelector('li.menu').onclick = () => alert('Meramente ilustrativo.');
+
+hamburguer.addEventListener('click', () => MenuMobile());
